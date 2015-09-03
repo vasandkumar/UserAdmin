32bdde6766c5c45a7aae55452ac8eb51337d94c7
vasandkumar <kvasandkumar@gmail.com>
2015-09-03 11:12:14 +0530

    Stylized the pics listing

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -5,10 +5,12 @@ class Pic < ActiveRecord::Base
 	has_attached_file :picture,
 					  :styles => {
                       	:thumb => "40x40>",
-                      	:small => "150x150>"
+                      	:small => "150x150>",
+                      	:thumbnail => "242x200>"
                     	}
 
-    validates :picture, :attachment_presence => true
+	validates :imageuri, presence: true
+	validates :picture, :attachment_presence => true
     validates_attachment_content_type :picture, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	belongs_to :user
